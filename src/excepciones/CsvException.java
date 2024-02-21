1fe47c0fca15b6a7ccbdcb7312bb3d78fe3bc824
Rubén Grueso Vega <ruben.gve@gmail.com>
2024-02-21 08:51:19 +0100

    CsvUtils actualizado y otrass cosillas

---
+++ src/excepciones/CsvException.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+public class CsvException extends JuegoException {
+
+    public CsvException(String msg) {
+        super(msg);
+    }
+
+    public CsvException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
