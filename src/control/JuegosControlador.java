22d1a719e7bf693b18e491263ab7ad579457caa2
dgimenezformacion <danieldocencia2023@gmail.com>
2024-02-21 10:13:24 +0100

    AltaJuegoControlador

--- src/control/JuegosControlador.java
+++ src/control/JuegosControlador.java
@@ -1,12 +1,45 @@
 package control;
 
+import excepciones.JuegoException;
+import model.Juego;
+import model.TipoGenero;
+import servicios.JuegosServicioImpl;
+import util.LeeDatos;
+
 public class JuegosControlador {
-	
+	JuegosServicioImpl servicio= new JuegosServicioImpl();
 		public void incio() {
 			
 		} 
 		public void cargarDatos() {
 			
 		}
+		public void altaJuego() {
+			try {
+				Juego juego= new Juego();
+				System.out.println("Introduce ranking");
+				int ranking=LeeDatos.leeInt();
+				System.out.println("Intrdoduce el nombre");
+				String nombre = LeeDatos.leeString();
+				System.out.println("Introduce la plataforma");
+				String plataforma = LeeDatos.leeString();
+				System.out.println("Intoduce fecha");
+				int añoFecha=LeeDatos.leeInt();
+				System.out.println("Introduce tipo genero");
+				TipoGenero tipoGenero=TipoGenero.valueOf(LeeDatos.leeString());
+				System.out.println("Introduce el editor");
+				String editor=LeeDatos.leeString();
+				juego.setRanking(ranking);
+				juego.setNombre(nombre);
+				juego.setPlataforma(plataforma);
+				juego.setFecha(añoFecha);
+				juego.setTipoGenero(tipoGenero);
+				juego.setEditor(editor);
+				servicio.cargarDatos(nombre);
+			}catch (JuegoException e) {
+				System.out.println("error");
+			}
+		}
+		
 	
 }
--- src/util/LeeDatos.java
+++ src/util/LeeDatos.java
@@ -1,5 +1,15 @@
 package util;
 
-public class LeeDatos {
+import java.util.Scanner;
 
+public class LeeDatos {
+	public static String leeString() {
+		Scanner lector = new Scanner(System.in);
+		return lector.nextLine();
+	}
+
+	public static int leeInt() {
+		Scanner lector = new Scanner(System.in);
+		return lector.nextInt();
+	}
 }
