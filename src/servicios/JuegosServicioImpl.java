2dc8e725fd61c4562bec81f69e3d78b3fbc56342
rafa <rafaelgarciaespada@gmail.com>
2024-02-21 10:07:38 +0100

    metodo altaJuego servicio

--- src/servicios/JuegosServicioImpl.java
+++ src/servicios/JuegosServicioImpl.java
@@ -1,6 +1,8 @@
 package servicios;
 
 import datos.DAOJuegosImp;
+import excepciones.JuegoException;
+import model.Juego;
 
 public class JuegosServicioImpl implements JuegosServicio {
 	DAOJuegosImp datos = new DAOJuegosImp();
@@ -8,6 +10,10 @@ public class JuegosServicioImpl implements JuegosServicio {
 	public void cargarDatos(String nombreFichero) {
 		//datos.cargarDatos("nombrefichero.txt");
 		System.out.println("Prueba");
+	}
+	
+	public void altaJuego(Juego juego) throws JuegoException {
+		datos.altaJuego(juego) ;
 	}
 
 }
